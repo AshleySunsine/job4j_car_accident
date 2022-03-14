c39cdada6bbb6b757f4ae5a9a59caf20959ccaf5
AshleySunsine <sgt16@yandex.ru>
2022-03-14 19:40:20 +0300

    0. Spring Security [#6879]01

--- src/main/java/ru/job4j/accident/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accident/repository/AccidentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accident.model.Accident;
 
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
+
 }
