2fa5163a717915b8a0c48456d75c443067dc62fe
AshleySunsine <sgt16@yandex.ru>
2022-03-20 18:59:27 +0300

    2. Spring Data [#296073 #274865]12

--- src/main/java/ru/job4j/accident/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accident/repository/AccidentRepository.java
@@ -2,14 +2,12 @@ package ru.job4j.accident.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.job4j.accident.model.Accident;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 @Transactional
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
     @Query("from Accident ac join fetch ac.rules join fetch ac.type")
